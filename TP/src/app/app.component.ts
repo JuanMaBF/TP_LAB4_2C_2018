735d15a3308ed6189a26970a3c095c72cf0a1179
Juanma Barreda Fafasuli <juanmanuelbf@gmail.com>
2018-10-24 18:02:03 -0300

    sumando el proyecto anterior

--- TP/src/app/app.component.ts
+++ TP/src/app/app.component.ts
@@ -3,7 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    
+    <header>
+      <nav class="navbar navbar-fixed-top">
+          <h2>La comanda</h2>
+      </nav>
+    </header>
+    <login></login>
   `,
   styleUrls: []
 })
